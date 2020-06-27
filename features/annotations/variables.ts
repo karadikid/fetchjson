af01d844ff15d49377f22a527569c625074def22
Gouthum Karadi <goukaradi@hotmail.com>
2020-06-27 05:48:43 -0400

    Added annotations

---
+++ features/annotations/variables.ts
@@ -0,0 +1,22 @@
+// Type
+let apples: number = 5;
+let speed: string = "fast";
+
+// Array
+let colors: string[] = ["red", "green", "blue"];
+
+// Classes
+class Car {}
+
+let car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20,
+};
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
