a7cc7d416db7a1e63309c7255853b5488714d89a
Gouthum Karadi <goukaradi@hotmail.com>
2020-06-27 06:08:46 -0400

    "Function returns any"

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -20,3 +20,15 @@ let point: { x: number; y: number } = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations
+// 1) Function returns the 'any' type
+
+const json = '{"x": 10, "y": 20}';
+
+const coordinates = JSON.parse(json);
+
+console.log(coordinates); // {x: 10, y:20};
+
+// Iterating over object by keys
+// https://dmitripavlutin.com/how-to-iterate-easily-over-object-properties-in-javascript/
